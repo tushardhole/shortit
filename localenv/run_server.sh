54eb7ed2e556b7d7ea707d7bdeeba36df5c9782f
Tushar Dhole <tushardhole@hotmail.com>
2019-12-09 10:57:31 +0530

    Fix installation script

--- localenv/run_server.sh
+++ localenv/run_server.sh
@@ -1,38 +1,33 @@
 #!/bin/sh
-
-dockerVersion=`docker --version`
-
-if [[ $dockerVersion == *"Docker version"* ]]; then
-  echo "Docker is already installed!"
-else
+docker --version
+if [ $? -ne 0 ]; then
   echo "Installing docker"
   brew cask install docker
-fi
-
-dockerComposeVersion=`docker-compose --version`
-
-if [[ $dockerComposeVersion == *"docker-compose version"* ]]; then
-  echo "Docker compose is already installed!"
 else
-  echo "Installing docker compose"
-  brew cask install docker-compose
+  echo "Docker is already installed!"
 fi
 
-dockerStat=`docker stats --no-stream`
+docker-compose --version
+if [ $? -ne 0 ]; then
+  echo "Installing docker compose"
+  brew install docker-compose
+else
+  echo "Docker compose is already installed!"
+fi
 
-if [ $? -ne 0 ]
-then
+docker stats --no-stream
+if [ $? -ne 0 ]; then
   echo "Docker is not running"
   open --background -a Docker
 else
   echo "Docker is already running"
 fi
 
-dockerStat=`docker stats --no-stream`
+docker stats --no-stream
 while [ $? -ne 0 ]
 do
-	echo "Waiting for docker to start"
-	docker stats --no-stream
+  echo "Waiting for docker to start"
+  docker stats --no-stream
 done
 
 docker-compose up
